02319ef2248bcecef3cfee3e38894306f058906e
Patrick Nafarrete <patrick@cheeseb.us>
2015-04-21 23:57:56 -0400

    Concise paragraphs are nice

--- radio/about/index.php
+++ radio/about/index.php
@@ -7,10 +7,9 @@
 <body>
     <div id="content">
         <h2>about cheeseb.us radio</h2>
-        <h6><a href="/radio/">cheeseb.us radio</a> cycles through a handpicked rotation of songs. the song database updates at least once a month.</h6>
-        <h6>albums prohibiting unauthorized copying and/or broadcasting are excluded from the rotation to the best extent possible.</h6>
-        <h6>on top of that, cheeseb.us radio operates on a NonCommercial basis.</h6>
-        <h6>the source code is available on <a href="https://github.com/pnaf/cheeseb.us/tree/master/radio">GitHub</a>.
+        <h6><a href="/radio/">cheeseb.us radio</a> cycles through a handpicked rotation of songs. the song database updates regularly.</h6>
+        <h6>albums explicitly prohibiting unauthorized copying and/or broadcasting are excluded from the rotation.</h6>
+        <h6>cheeseb.us radio operates on a NonCommercial basis. the source code is available on <a href="https://github.com/pnaf/cheeseb.us/tree/master/radio">GitHub</a>.
         <h6>other questions? <a href="mailto:admin@cheeseb.us">email</a></h6>
     </div>
 </body>
