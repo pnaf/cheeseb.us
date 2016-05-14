ddc24698cea55bc3358ad30246efe3e7c027ff5e
Patrick Nafarrete <patrick@cheeseb.us>
2016-05-13 22:06:57 -0400

    Match against "linux" instead of "linux x86_64"

--- radio/index.php
+++ radio/index.php
@@ -1,5 +1,5 @@
 <?php
-if (preg_match("/\blinux x86_64\b/i", $_SERVER['HTTP_USER_AGENT'])) {
+if (preg_match("/\blinux\b/i", $_SERVER['HTTP_USER_AGENT'])) {
 include 'xspf.php';
 } elseif (preg_match("/firefox/i", $_SERVER['HTTP_USER_AGENT'])) {
 include 'm3u.php';
