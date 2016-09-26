130b615dcb280d3cf7c0dcc2fca22bbe4cddb349
Patrick Nafarrete <patrick@cheeseb.us>
2016-09-25 23:54:00 -0400

    Redirect Safari users to fallback page

--- radio/index.php
+++ radio/index.php
@@ -1,5 +1,7 @@
 <?php
-if (preg_match("/\blinux\b/i", $_SERVER['HTTP_USER_AGENT'])) {
+if (preg_match("/safari/i", $_SERVER['HTTP_USER_AGENT'])) {
+include 'fallback.php';
+} elseif (preg_match("/\blinux\b/i", $_SERVER['HTTP_USER_AGENT'])) {
 include 'xspf.php';
 } elseif (preg_match("/firefox/i", $_SERVER['HTTP_USER_AGENT'])) {
 include 'm3u.php';
